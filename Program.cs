d9ebda88e207ffa5a266aeab4cd6a407a55b5d8e
Kento Shirasawa <shirasawa.kento@gmail.com>
2022-02-08 23:46:52 +0900

    learn: multi array

--- Program.cs
+++ Program.cs
@@ -64,6 +64,29 @@ namespace learn_csharp
             {
                 Console.WriteLine(i);
             }
+            // multidimensional array
+            /*
+             * 2d: int[,] a = new int[3,4];       -> a[1,2] = 2;
+             * 3d: int[,,] a = new int[3,4,5]     -> a[1,2,3] = 2;
+             * 4d: int[,,,] a = new int[3,4,5,6]  -> a[1,2,3,4] = 2;
+             */
+            int[,] mArray = new int[3, 4]; 
+            int m, n;
+            for (m = 0; m < 3; m++)
+            {
+                for (n = 0; n < 4; n++)
+                {
+                    mArray[m, n] = m + n;
+                }
+            }
+            for (m = 0; m < 3; m++)
+            {
+                for (n = 0; n < 4; n++)
+                {
+                    Console.Write("a[{0},{1}]={2} ", m, n, mArray[m, n]);
+                }
+                Console.WriteLine();
+            }
             // class
             Console.WriteLine("-class---------");
             var User = new User();
